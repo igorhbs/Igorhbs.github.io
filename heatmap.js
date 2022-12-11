e0a027a238df30db0b748ddedc2f26ed5590f779
igorhbs <46987492+igorhbs@users.noreply.github.com>
2022-12-11 00:40:15 -0300

    Update heatmap.js

--- heatmap.js
+++ heatmap.js
@@ -1,26 +1,24 @@
-//Create the SVG and set the width and height variables
-var svg = d3.select("body")
-    .append("svg")
-    .attr("width", 500)
-    .attr("height", 500);
+// Declaração do d3
+const d3 = require('d3');
 
-//Create the data and set the color scale
-var dataset = [
-    [5, 20], [480, 90], [250, 50], [100, 33], [330, 95],
-    [410, 12], [475, 44], [25, 67], [85, 21], [220, 88]
-];
-
-var colors = d3.scaleLinear()
-    .domain([0, d3.max(dataset, function(d) { return d[1]; })])
-    .range(["rgb(0,0,255)", "rgb(255,0,0)"]);
-
-//Add the rectangles for the heatmap
-svg.selectAll("rect")
-    .data(dataset)
-    .enter()
-    .append("rect")
-    .attr("x", function(d) { return d[0]; })
-    .attr("y", function(d) { return d[1]; })
-    .attr("width", 20)
-    .attr("height", 20)
-    .style("fill", function(d) { return colors(d[1]); });
+// chamada para a API
+d3.json("https://dadosabertos.senado.leg.br/api/v2/senadores",
+  function(error, data) {
+    if (error) {
+      console.log(error);
+      return;
+    }
+    
+    // mapear o JSON para retornar os partidos
+    var senadoresByPartido = d3.nest()
+      .key(function(d) { return d.ultimoStatus.partido.sigla; })
+      .entries(data.dados);
+    
+    // criar os botões para cada partido
+    var partidosButtons = d3.select("#chart")
+      .selectAll("input")
+      .data(senadoresByPartido)
+      .enter()
+      .append("input")
+      .attr("type", "button")
+      .attr("value", function(d) { return d.key; });
